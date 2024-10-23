05993b9c8589a7dfe3e09c559776b262010a3425
Dmitri <gorsh03@gmail.com>
2024-10-23 18:37:48 +0400

    create repo

---
+++ crops_classifier.py
@@ -0,0 +1,320 @@
+import numpy as np
+import geopandas as gpd
+import pandas as pd
+import datetime
+import json
+import asyncio
+import logging
+import time
+import argparse
+from joblib import dump, load
+from os import path
+from asyncio import TimeoutError
+from scipy.interpolate import InterpolatedUnivariateSpline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from ndvi_download_service import AgroFieldService
+
+TODAY = datetime.datetime.today().strftime('%Y-%m-%d_%H-%M')
+LOG_PATH = f'logs/crops_{TODAY}.log'
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_PATH, encoding='utf-8'),
+        logging.StreamHandler()
+    ])
+
+logger = logging.getLogger('CropsClassifier')
+
+
+class AgroClassifierService:
+    """
+    Класс AgroClassifier отвечает за загрузку и обработку данных NDVI,
+    обучение модели классификации и классификацию данных на основе обученной модели.
+    """
+
+    def __init__(self, config_file: str) -> None:
+        """
+        Инициализация AgroClassifier и загрузка конфигурации из указанного файла.
+
+        Args:
+            config_file: Путь к файлу конфигурации JSON.
+        """
+        self.config = self.__load_config__(config_file)
+        if not self.config.get('year'):
+            raise ValueError('The year is not specified in the configuration')
+        self.label_dict = {'озимые': 0, 'многолетние травы': 1,
+                           'ранние яровые': 2, 'поздние яровые': 3, 'пар': 4}
+        self.label_inv = {0: 'озимые', 1: 'многолетние травы',
+                          2: 'ранние яровые', 3: 'поздние яровые', 4: 'пар'}
+        logger.info(f"Initialization AgroClassifier")
+
+    def __load_config__(self, config_file: str) -> dict:
+        """
+        Загрузка конфигурации из JSON файла.
+
+        Args:
+            config_file: Путь к файлу конфигурации JSON.
+
+        Raises:
+            FileNotFoundError: Если файл конфигурации не найден.
+            json.JSONDecodeError: Если произошла ошибка декодирования JSON.
+
+        Returns:
+            Конфигурационный словарь.
+        """
+        try:
+            with open(config_file, 'r') as file:
+                config = json.load(file)
+                logger.info(f"Configuration loaded from {config_file}")
+                return config
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            logger.error(f"Error loading configuration: {e}")
+            raise
+
+    def __create_split__(self, features: list, labels: list, random_state: int) -> \
+            tuple[np.array, np.array, np.array, np.array]:
+        """ 
+        Разделяет данные на обучающую и тестовую выборки.
+
+        Args:
+            features: Список признаков.
+            labels: Список меток.
+            random_state: Случайное состояние для воспроизводимости.
+
+        Returns:
+            tuple[np.array, np.array, np.array, np.array]: Кортеж из X_train, X_test, y_train, y_test.
+        """
+        X_train, X_test, y_train, y_test = train_test_split(
+            features, labels, test_size=0.25, random_state=random_state * 17)
+        return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
+
+    async def __download_ndvi_async__(self, geojson_file: str, year: int) -> tuple[gpd.GeoDataFrame, pd.DataFrame]:
+        """
+        Асинхронно загружает данные NDVI для указанного geojson файла и года.
+
+        Args:
+            geojson_file (str): Путь к geojson файлу.
+            year (int): Год, для которого загружаются данные.
+
+        Raises:
+            TimeoutError: Если произошел таймаут при загрузке данных.
+            RuntimeError: Если произошла ошибка выполнения в asyncio.
+
+        Returns:
+            tuple[gpd.GeoDataFrame, pd.DataFrame]: Кортеж из GeoDataFrame и DataFrame с данными NDVI.
+        """
+        afs = AgroFieldService(self.config)
+        afs.clear_data()
+        try:
+            await afs.compute_ndvi(geojson_file)
+        except TimeoutError as e:
+            logger.error(f"Error loading data: {e}")
+            raise
+        except RuntimeError:
+            pass
+        gdf = gpd.read_file(geojson_file)
+        gdf['Date'] = year
+        df_series = afs.read_data()
+        afs.clear_data()
+        return gdf, df_series
+
+    async def __download_ndvi__(self, geojson_file: str, year: int) -> tuple[gpd.GeoDataFrame, pd.DataFrame]:
+        """
+        Обертка для асинхронной загрузки данных NDVI.
+
+        Args:
+            geojson_file (str): Путь к geojson файлу.
+            year (int): Год, для которого загружаются данные.
+
+        Returns:
+            tuple[gpd.GeoDataFrame, pd.DataFrame]: Кортеж из GeoDataFrame и DataFrame с данными NDVI.
+        """
+        logger.info(f"Loading NDVI for file {geojson_file} and year {year}")
+        return await self.__download_ndvi_async__(geojson_file, year)
+
+    def __process_ndvi_data__(self, df_series: pd.DataFrame, gdf: gpd.GeoDataFrame, year: int, flag: bool) -> \
+            tuple[np.array, (np.array, list)]:
+        """
+        Обрабатывает данные NDVI, выполняет интерполяцию и формирует признаки и метки.
+
+        Args:
+            df_series: DataFrame с данными NDVI.
+            gdf: GeoDataFrame с геометрическими данными.
+            year: Год для обработки данных.
+
+        Returns:
+            tuple[np.array, (np.array or list)]: Кортеж из массива признаков и массива меток.
+        """
+        features, labels, obj_ids = [], [], []
+        veg_start_date = datetime.date(year, 4, 1).toordinal()
+        veg_end_date = datetime.date(year, 9, 1).toordinal()
+
+        for key in df_series['ID'].unique():
+            X = df_series.loc[df_series['ID'] ==
+                              key, 'Date'].to_numpy().flatten()
+            Y = df_series.loc[df_series['ID'] ==
+                              key, 'NDVI'].to_numpy().flatten()
+
+            X_ordinals = np.array([i.toordinal() for i in X[::-1]])
+            Y = Y[::-1]
+
+            if Y[0] < 0:
+                X_ordinals[0] = veg_start_date
+                Y[0] = 0
+            for i in range(1, len(Y) - 1):
+                if Y[i - 1] > 0 and Y[i] < 0 and Y[i + 1] > 0:
+                    Y[i] = np.mean((Y[i - 1], Y[i + 1]))
+
+            newX = np.arange(veg_end_date - veg_start_date)
+            X_ordinals -= veg_start_date
+            s = InterpolatedUnivariateSpline(
+                np.sort(X_ordinals), Y[np.argsort(X_ordinals)], k=1)
+            newY = s(newX)
+
+            features.append(newY)
+            if flag:
+                label = gdf.loc[(gdf['ID'] == key), 'CropClass'].values
+                label_str = ''.join(label).translate(
+                    {ord(i): None for i in '\'[]'})
+                labels.append(self.label_dict[label_str])
+            else:
+                obj_ids.append(key)
+
+        if flag:
+            return np.array(features, dtype='float'), np.array(labels)
+        else:
+            return np.array(features, dtype='float'), obj_ids
+
+    def model_training(self) -> tuple[RandomForestClassifier, float]:
+        """
+        Обучает модель классификации на основе данных NDVI.
+
+        Raises:
+            Exception: Если произошла ошибка во время обучения модели.
+
+        Returns:
+            tuple[RandomForestClassifier, float]: Кортеж из обученной модели RandomForestClassifier и её точности.
+        """
+        if not path.exists(self.config['train_geojson_file']):
+            raise FileNotFoundError('Training file was not found')
+        geojson_file = self.config['train_geojson_file']
+        year = self.config['year']
+        logger.info(
+            f"Starting model training for file {geojson_file} and year {year}")
+
+        try:
+            gdf, df_series = asyncio.run(self.__download_ndvi__(
+                geojson_file, year))
+            asyncio.run(asyncio.sleep(2))
+            features, labels = self.__process_ndvi_data__(
+                df_series, gdf, year, flag=True)
+            logger.info("Feature extraction and data labeling completed")
+
+            counter = 10
+            models = []
+            accuracys = np.zeros((counter, 1), dtype='float32')
+
+            for i in range(counter):
+                X_train, X_test, y_train, y_test = self.__create_split__(
+                    features, labels, i)
+                rfc = RandomForestClassifier(
+                    max_depth=13, n_estimators=143, random_state=i)
+                rfc_model = rfc.fit(X_train, y_train)
+                preds = rfc.predict(X_test)
+                accuracy = accuracy_score(y_test, preds)
+                accuracys[i] = accuracy
+                models.append(rfc_model)
+                logger.info(f"Model {i+1} trained with accuracy {accuracy:.4f}")
+
+            best_index = np.argmax(accuracys)
+            model = models[best_index]
+            logger.info(
+                f"The best model is trained with accuracy {accuracys[best_index][0]:.4f}")
+            model_path = f"models/model_{datetime.datetime.today().strftime('%Y-%m-%d_%H-%M-%S')}.joblib"
+            dump(model, model_path, compress=9)
+            dump(model, 'models/last.joblib', compress=9)
+            return model, accuracys[best_index][0]
+
+        except Exception as e:
+            logger.error(f"Error during model training: {e}")
+            raise
+
+    def classify_data(self) -> None:
+        """
+        Классифицирует данные NDVI на основе обученной модели и сохраняет результаты в новый geojson файл.
+
+        Raises:
+            Exception: Если произошла ошибка во время классификации данных.
+        """
+        if not path.exists(self.config['classify_geojson_file']):
+            raise FileNotFoundError('Training file was not found')
+        if not path.exists(self.config['new_geojson_file']):
+            raise FileNotFoundError('New path to file was not found')
+        geojson_file = self.config['classify_geojson_file']
+        year = self.config['year']
+        model_path = self.config['model']
+        logger.info(f"Data classification for file {geojson_file}")
+
+        try:
+            if path.exists(model_path):
+                model = load(model_path)
+                logger.info(f"Model loaded from {model_path}")
+            else:
+                raise FileNotFoundError(
+                    f"Model file {model_path} not found")
+
+            gdf, df_series = asyncio.run(
+                self.__download_ndvi__(geojson_file, year))
+            asyncio.run(asyncio.sleep(2))
+            new_features, obj_ids = self.__process_ndvi_data__(
+                df_series, gdf, year, flag=False)
+
+            preds = model.predict(new_features)
+            preds_decoded = [self.label_inv[pred] for pred in preds]
+            gdf['Name'] = None
+
+            for obj_id in obj_ids:
+                indx = obj_ids.index(obj_id)
+                gdf.loc[gdf['ID'] == obj_id, 'Name'] = preds_decoded[indx]
+
+            gdf.drop(columns=['Date'], inplace=True)
+            gdf.to_file(self.config['new_geojson_file'], driver='GeoJSON')
+            logger.info(f"Classification completed for file {geojson_file}")
+
+        except Exception as e:
+            logger.error(f"Error during classification: {e}")
+            raise
+
+
+def main(config_path, mode):
+    start_time = time.time()
+    ac = AgroClassifierService(config_path)
+
+    if mode == 'train':
+        try:
+            ac.model_training()
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+    if mode == 'classify':
+        try:
+            ac.classify_data()
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
+
+    end_time = time.time()
+    logger.info(f"Running time: {end_time - start_time:.2f} seconds")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='AgroClassifier Service')
+    parser.add_argument('--config', type=str, required=False, default='configuration.json',
+                        help='Path to the configuration file (configuration.json is used by default)')
+    parser.add_argument('--mode', type=str, required=False, choices=['classify', 'train'], default='train',
+                        help='Running a method (classify - classification, train - training)')
+
+    args = parser.parse_args()
+    main(args.config, args.mode)
---
+++ ndvi_download_service.py
@@ -0,0 +1,278 @@
+import asyncio
+import aiohttp
+import json
+import pandas as pd
+import geopandas as gpd
+import logging
+import datetime
+from tqdm.asyncio import tqdm
+from json.decoder import JSONDecodeError
+from psycopg2 import pool, OperationalError
+from shapely.geometry import shape
+from shapely.validation import make_valid
+
+TODAY = datetime.datetime.today().strftime('%Y-%m-%d_%H-%M')
+LOG_PATH = f'logs/crops_{TODAY}.log'
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_PATH, encoding='utf-8'),
+        logging.StreamHandler()
+    ])
+
+logger = logging.getLogger(__name__)
+
+
+class AgroFieldService:
+    """
+    Сервис для работы с данными NDVI.
+    Выполняет загрузку данных, их обработку, сохранение в базу данных и чтение из базы данных.
+    """
+
+    INSERT_NDVI_QUERY = '''
+    INSERT INTO agrofieldndvi ("AgroFieldID", "Date", "NDVI", "NDVIMin", "NDVIMax")
+    VALUES (%s, %s, %s, %s, %s);
+    '''
+
+    DELETE_NDVI_QUERY = '''
+    DELETE FROM agrofieldndvi;
+    '''
+
+    READ_NDVI_QUERY = '''
+    SELECT "AgroFieldID", "NDVI", "Date" FROM agrofieldndvi WHERE EXTRACT(YEAR FROM "Date") = %s;
+    '''
+
+    NDVI_FIS_URL = 'https://functions.yandexcloud.net/d4e1flb0tjah7plqhdjf?LAYER=NDVISRC&STYLE=INDEX&CRS=EPSG%3A3857&TIME={1}-04-01%2F{1}-09-01&GEOMETRY={0}&RESOLUTION=50'
+
+    def __init__(self, config: dict) -> None:
+        """
+        Инициализация сервиса.
+
+        :param config: Словарь с конфигурацией.
+        """
+        self.db_config = config['database']
+        self.persecond = config['persecond'] if config['persecond'] else 20
+        self.year = config['year']
+        self.connection_pool = pool.ThreadedConnectionPool(
+            5, 100, **self.db_config)
+        self.__check_db_connection__()
+
+    def __check_db_connection__(self) -> None:
+        """
+        Проверка соединения с базой данных.
+
+        :raises OperationalError: Если соединение с базой данных не удалось установить.
+        """
+        try:
+            conn = self.connection_pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute("SELECT version();")
+            record = cursor.fetchone()
+            if record:
+                logger.info(f"Database connection: {record} established successfully")
+        except OperationalError as e:
+            logger.error(f"Database connection error: {e}")
+            raise
+        finally:
+            cursor.close()
+            self.connection_pool.putconn(conn)
+
+    def __process_geometry__(self, geom) -> str:
+        """
+        Обрабатывает геометрию для формирования корректного WKT.
+
+        :param geom: Геометрия объекта.
+        :return (str): Обработанная геометрия в формате WKT.
+        """
+        valid_geom = make_valid(geom)
+        simplified_geom = valid_geom.simplify(
+            tolerance=100, preserve_topology=True)
+        buffered_geom = simplified_geom.buffer(0.01)
+        buffered_wkt = buffered_geom.wkt
+        if len(buffered_wkt) > 1000:
+            envelope_geom = valid_geom.envelope
+            return envelope_geom.wkt
+        else:
+            return buffered_wkt
+
+    async def __fetch__(self, session: aiohttp.ClientSession, task: dict) -> None:
+        """
+        Асинхронно выполняет HTTP-запрос для получения данных NDVI и сохраняет их в базу данных.
+
+        :param session: Сессия aiohttp для выполнения запросов.
+        :param task: Словарь с информацией о задаче, содержащий 'url' и 'ID'.
+        """
+        url = task['url']
+        retries = 0
+        max_retries = 5
+        retry_delay = 0.5
+
+        while retries < max_retries:
+            try:
+                async with session.get(url) as response:
+                    if response.status == 429:
+                        wait_time = retry_delay * (2 ** retries)
+                        logger.warning(
+                            f"Error 429: Waiting {wait_time} seconds before trying {url} again...")
+                        await asyncio.sleep(wait_time)
+                        retries += 1
+                        continue
+                    elif response.status != 200:
+                        logger.error(
+                            f"Failed to get {url}. Status code: {response.raise_for_status()}")
+                        task['result'] = None
+                        task['status'] = 'failed'
+                        return
+
+                    task['result'] = await response.text()
+                    logger.info(task['result'])
+                    task['status'] = 'done'
+                    sentinel_data = json.loads(task['result'])
+                    agro_field_id = task['ID']
+                    await self.__save_ndvi_data__(agro_field_id, sentinel_data)
+                    return
+
+            except JSONDecodeError as e:
+                logger.error(f"JSONDecoder error for {url}: {e}")
+                retries += 1
+
+            except aiohttp.ClientError as e:
+                logger.error(f"{url} request error: {e}")
+                retries += 1
+        logger.error(
+            f"The maximum number of retries for {url} has been reached. Pass...")
+        task['result'] = None
+        task['status'] = 'failed'
+        return
+
+    async def __fetch_all__(self, session: aiohttp.ClientSession, urls: list[dict]) -> list[dict]:
+        """
+        Асинхронно выполняет все задачи в очереди по загрузке данных NDVI.
+
+        :param session: Сессия aiohttp для выполнения запросов.
+        :param urls: Список словарей с 'ID' и 'url' для загрузки данных.
+        :return: Обновленный список задач с результатами.
+        """
+        url_tasks = [{'ID': i['_ID'], 'url': i['url'],
+                      'result': None, 'status': 'new'} for i in urls]
+        n = 0
+
+        total_tasks = len(url_tasks)
+
+        with tqdm(total=total_tasks, desc="Fetching NDVI data", unit="task") as progress:
+            while True:
+                running_tasks = len(
+                    [i for i in url_tasks if i['status'] == 'fetch'])
+                is_tasks_to_wait = len(
+                    [i for i in url_tasks if (i['status'] != 'done' or i['status'] != 'failed')])
+
+                if n < len(url_tasks) and running_tasks < self.persecond:
+                    url_tasks[n]['status'] = 'fetch'
+                    asyncio.create_task(self.__fetch__(session, url_tasks[n]))
+                    n += 1
+                    # logger.info(f"Scheduled task {n}. Running: {running_tasks}, Remaining: {is_tasks_to_wait}")
+
+                if running_tasks >= self.persecond:
+                    await asyncio.sleep(1)
+
+                completed_or_failed_tasks = len([i for i in url_tasks if (i['status'] == 'done' or i['status'] == 'failed')])
+                progress.n = completed_or_failed_tasks
+                progress.refresh()
+                
+                if is_tasks_to_wait != 0:
+                    await asyncio.sleep(0.1)
+                else:
+                    break
+
+        return url_tasks
+
+    async def compute_ndvi(self, geojson_file: str) -> gpd.GeoDataFrame:
+        """
+        Загружает и обрабатывает данные NDVI для указанного geojson файла и года.
+
+        :param geojson_file: Путь к geojson файлу с геометрией полей.
+        :return: GeoDataFrame с геометрией и добавленными данными NDVI.
+        """
+        gdf = gpd.read_file(geojson_file)
+        urls = []
+
+        for _, row in gdf.iterrows():
+            geom = shape(row['geometry'])
+            processed_wkt = self.__process_geometry__(geom)
+            url = self.NDVI_FIS_URL.format(processed_wkt, self.year)
+            urls.append({'_ID': row['ID'], 'url': url})
+
+        logger.info(
+            f"NDVI parsing begins for {self.year} with fields {len(urls)}.")
+
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            await self.__fetch_all__(session, urls)
+        return gdf
+
+    async def __save_ndvi_data__(self, agro_field_id: int, sentinel_data: dict) -> None:
+        """
+        Сохраняет полученные данные NDVI в базу данных.
+
+        :param agro_field_id: Идентификатор агро поля.
+        :param sentinel_data: Данные NDVI, полученные из ответа API.
+        """
+        try:
+            conn = self.connection_pool.getconn()
+            cursor = conn.cursor()
+            for date in sentinel_data['C0']:
+                cursor.execute(self.INSERT_NDVI_QUERY,
+                               (agro_field_id,
+                                date['date'],
+                                date['basicStats']['mean'],
+                                date['basicStats']['min'],
+                                date['basicStats']['max']))
+            # logger.info(f"{len(sentinel_data['C0'])} NDVI records saved for AgroFieldID {agro_field_id}")
+            conn.commit()
+        except Exception as e:
+            logger.error(
+                f"Error saving NDVI series for AgroFieldID {agro_field_id}: {e}")
+            raise
+        finally:
+            cursor.close()
+            self.connection_pool.putconn(conn)
+
+    def clear_data(self) -> None:
+        """
+        Удаляет все записи NDVI из базы данных.
+        """
+        try:
+            conn = self.connection_pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(self.DELETE_NDVI_QUERY)
+            conn.commit()
+            logger.info(f"Database entries were successfully deleted")
+        except Exception as e:
+            logger.error(f"Database cleaning error: {e}")
+            raise
+        finally:
+            cursor.close()
+            self.connection_pool.putconn(conn)
+
+    def read_data(self) -> pd.DataFrame:
+        """
+        Читает данные NDVI из базы данных для заданного года.
+
+        :return: DataFrame с колонками 'ID', 'NDVI', 'Date'.
+        """
+        try:
+            conn = self.connection_pool.getconn()
+            cursor = conn.cursor()
+            cursor.execute(self.READ_NDVI_QUERY, (self.year,))
+            df = pd.DataFrame(cursor.fetchall(), columns=[
+                              'ID', 'NDVI', 'Date'])
+            df = df.dropna()
+            logger.info(f"Records from the database were successfully read")
+        except Exception as e:
+            logger.error(f"Reading error: {e}")
+            raise
+        finally:
+            cursor.close()
+            self.connection_pool.putconn(conn)
+        return df
